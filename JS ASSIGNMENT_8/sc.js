2992ac6468a6856a1b441f043cca24df5b3bf2a7
LakshmipriyaSreedharan <lakshmipriya21comp@student.mes.ac.in>
2024-06-04 13:06:26 +0530

    Add files via upload

---
+++ JS ASSIGNMENT_8/sc.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("https://my-json-server.typicode.com/FreSauce/json-ipl/data")
+      .then((response) => response.json())
+      .then((data) => {
+        // Sort the data based on NRR value
+        data.sort((a, b) => a.NRR - b.NRR);
+  
+        // Get the table body
+        const tbody = document.querySelector("tbody");
+  
+        // Loop through the sorted data and generate table rows
+        data.forEach((team, index) => {
+          const row = document.createElement("tr");
+          row.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${team.Team}</td>
+            <td>${team.Matches}</td>
+            <td>${team.Won}</td>
+            <td>${team.Lost}</td>
+            <td>${team.Tied}</td>
+            <td>${team.NRR}</td>
+            <td>${team.Points}</td>
+          `;
+          tbody.appendChild(row);
+        });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  });
+  
